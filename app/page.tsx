5a287fc40214351445b56de13cefaedba913acfa
Jarrod Akcelo <jarrod.sterling@akcelo.com>
2024-12-10 12:54:05 -0800

    iframe test

--- app/page.tsx
+++ app/page.tsx
@@ -1,89 +1,13 @@
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
-
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-3xl font-semibold">Jarrod Sterling</h1>
-      <h4 className="mb-8 text-lg font-semibold">
-        {`a `}
-        <a
-          href="https://www.simplilearn.com/what-is-a-ui-developer-and-what-skills-are-required-article"
-          target="_blank"
-        >
-          UI
-        </a>
-        {` / `}
-        <a href="https://linkedin.com/in/jsterlingdev" target="_blank">
-          Front-end develop
-        </a>
-        {` from Vancouver, Canada.`}
-      </h4>
-      <p className="mb-4">
-        {`I love all things HTML, CSS & JavaScript.`} <br />
-        {`Currently enjoying working in front-end frameworks like `}
-        <a href="https://nextjs.org/docs" target="_blank">
-          Next.js
-        </a>
-        {` & Tailwind.`}
-        <br />
-        <br />
-        {`I work at a rad agency based out of `}
-        <a href="https://akcelo.com/#philosophy" target="_blank">
-          Australia
-        </a>
-        {`.`}
-        <br />
-        {`Client highlights include Spotify, TikTok, McDonalds & Pepsi.`}
-        <br />
-        <br />
-        {`In my spare time you can find me exploring `}
-        <a
-          href="https://en.wikipedia.org/wiki/English_Bay,_Vancouver"
-          target="_blank"
-        >
-          Vancouver
-        </a>
-        {` on my bike, swimming at my `}
-        <a href="https://en.wikipedia.org/wiki/Third_Beach" target="_blank">
-          favourite beach
-        </a>
-        {` or writing/recording `}
-        <a href="https://www.instagram.com/j.jsterling" target="_blank">
-          music
-        </a>
-        {`.`}
-      </p>
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <p>
-            I love connecting with people. Feel free to drop a line and say hi ~
-          </p>
-          <br />
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            target="_blank"
-            href="mailto:create@jarrodsterling.com"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">create@jarrodsterling.com</p>
-          </a>
-        </li>
-      </ul>
-    </section>
+    <>
+      <iframe
+        style="border: 1px solid rgba(0, 0, 0, 0.1);"
+        width="800"
+        height="450"
+        src="https://embed.figma.com/proto/LnQXs8ZL4GA68ZO67MTQml/F2F-%E2%80%93%C2%A06-Pillars-for-Success?page-id=601%3A1737&amp;node-id=601-1738&amp;node-type=canvas&amp;viewport=-893%2C812%2C1.16&amp;scaling=min-zoom&amp;content-scaling=fixed&amp;starting-point-node-id=601%3A1738&amp;embed-host=share"
+        allowfullscreen="0"
+      ></iframe>
+    </>
   )
 }
